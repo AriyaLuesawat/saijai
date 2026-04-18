ae880244fcb5ce305fee88602fde6d5887c3430f
AriyaLuesawat <141659011+AriyaLuesawat@users.noreply.github.com>
2026-04-18 20:20:57 +0700

    Update page.tsx

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -241,13 +241,7 @@ export default function Home() {
         const fd = new FormData();
         fd.append("file", file);
         try {
-          const res = await fetch(`https://goofiness-clapping-elevator.ngrok-free.dev/analyze-slip/`, { 
-            method: "POST", 
-            body: fd,
-            headers: {
-            "ngrok-skip-browser-warning": "true" 
-          }
-    q    });
+          const res  = await fetch(`https://goofiness-clapping-elevator.ngrok-free.dev/analyze-slip/`, { method: "POST", body: fd });
           const data = await res.json();
           if (data.status === "success") {
             results.push({
